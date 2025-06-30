e294b42d9de9906d94f621305ac5454f371d44c0
quannc4 <quannc4@fpt.com>
2025-06-30 15:25:31 +0700

    feat: Add animated beautiful cake

--- script.js
+++ script.js
@@ -43,4 +43,52 @@ function showSlides() {
 }
 
 createSlides();
-showSlides(); 
+showSlides();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.fireworks-container');
+    const fireworks = new Fireworks.default(container, {
+        rocketsPoint: {
+            min: 50,
+            max: 50
+        },
+        hue: {
+            min: 0,
+            max: 360
+        },
+        delay: {
+            min: 0.015,
+            max: 0.03
+        },
+        speed: 2,
+        acceleration: 1.05,
+        friction: 0.95,
+        gravity: 1.5,
+        particles: 50,
+        traceLength: 3,
+        flickering: 50,
+        opacity: 0.5,
+        explosion: 5,
+        intensity: 5,
+        lineWidth: {
+            explosion: {
+                min: 1,
+                max: 3
+            },
+            trace: {
+                min: 0.1,
+                max: 1
+            }
+        },
+        mouse: {
+            click: false,
+            move: false,
+            max: 1
+        }
+    });
+
+    fireworks.start();
+    setTimeout(() => {
+        fireworks.stop();
+    }, 8000); // Stop fireworks after 8 seconds
+}); 
